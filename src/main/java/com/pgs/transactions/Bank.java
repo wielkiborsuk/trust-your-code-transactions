54d963f48020a3dcc9c4e512cbf4493ddeb23a6a
wielki.borsuk <wielki.borsuk@gmail.com>
2021-05-24 06:23:46 +0200

    add bank transfer guarantees

--- src/main/java/com/pgs/transactions/Bank.java
+++ src/main/java/com/pgs/transactions/Bank.java
@@ -3,13 +3,24 @@ package com.pgs.transactions;
 class Bank {
 
     //@ requires amount >= 0;
+    //@ requires src != dst;
+    //@ ensures !\result ==> \not_modified(src.balance) && \not_modified(dst.balance);
+    //@ ensures \result ==> src.balance == \old(src.balance) - amount;
+    //@ ensures \result ==> dst.balance == \old(dst.balance) + amount;
     boolean transfer(Account src, Account dst, int amount) {
         try {
             src.withdraw(amount);
             dst.deposit(amount);
             return true;
-        } catch (NotEnoughMoneyException | TooMuchMoneyException e) {
+        } catch (NotEnoughMoneyException e) {
             return false;
+        } catch (TooMuchMoneyException e) {
+            try {
+                src.deposit(amount);
+            } catch (TooMuchMoneyException ex) {
+                return false;
+            }
         }
+        return false;
     }
 }
